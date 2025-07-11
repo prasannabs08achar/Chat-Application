045fe7acd869e52d4e3a52636be54889890902dc
prasa08nna <prasannabsachar@gmail.com>
2025-07-12 00:18:20 +0530

    Socket frontend part done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfiePage from './pages/ProfiePage'
+import {Toaster} from 'react-hot-toast'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 const App = () => {
+  const  {authUser}=useContext(AuthContext); 
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster/>
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/profile' element={<ProfiePage/>}/>
+      <Route path='/' element={authUser?<HomePage/>:<Navigate to='/login'/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />}/>
+        <Route path='/profile' element={authUser ? <ProfiePage /> : <Navigate to='/login' />}/>
     </Routes>
     </div>
   )
