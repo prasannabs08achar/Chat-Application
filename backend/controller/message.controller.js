d4b80e113418115d976c6c8cb1b485dbcda70a5e
prasa08nna <prasannabsachar@gmail.com>
2025-07-12 18:43:25 +0530

    Chat application project done

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -9,14 +9,24 @@ const getUserForSidebar=async(req,res)=>{
         const userId=req.user._id;
         const filterdUser=await User.find({_id:{$ne:userId}}).select('-password');
         const unseenMessages={}
-        const promises=filterdUser.map(async(user)=>{
-            const messages=await Message.find({senderId:user._id,receiverId:userId,seen:false}).countDocuments();
-            if(messages.length>0){
-                unseenMessages[user._id]=messages.length;
+        const promises = filterdUser.map(async (user) => {
+            const count = await Message.countDocuments({
+                senderId: user._id,
+                receiverId: userId,
+                seen: false,
+            });
+            if (count > 0) {
+                unseenMessages[user._id] = count;
             }
-        })
-        await Promise.all(promises);    
-        return res.json({success:true, users:filterdUser, message:"Users fetched successfully"})
+        });
+        await Promise.all(promises);
+
+        return res.json({
+            success: true,
+            users: filterdUser,
+            unseenMessages,
+            message: "Users fetched successfully",
+        });
     } catch (error) {
         return res.json({success:false, message:error.message})
     }
@@ -27,7 +37,10 @@ const getMessages=async(req,res)=>{
         const userId=req.user._id;
         const {id :selectedUserId}=req.params;
         const messages=await Message.find({$or:[{senderId:userId,receiverId:selectedUserId},{senderId:selectedUserId,receiverId:userId}]});
-        await Message.updateMany({senderId:userId,receiverId:selectedUserId},{seen:true});
+        await Message.updateMany(
+            { senderId: selectedUserId, receiverId: userId, seen: false },
+            { $set: { seen: true } }
+        );
         return res.json({success:true, messages, message:"Messages fetched successfully"}) 
     } catch (error) {
         return res.json({ success: false, message: error.message })
@@ -48,12 +61,12 @@ const markMessageAsSeen=async(req,res)=>{
 const sendMessage=async(req,res)=>{
     try {
         const {text,image}=req.body;
-        const {receiverId}=req.params.id;
+        const receiverId=req.params.id;
         const senderId=req.user._id;
         let imageUrl;
-        if(image){
-            const imageUrl=uploadCloudinary(image);
-            imageUrl=imageUrl.url
+        if (image) {
+            const uploadRes = await uploadCloudinary(image);
+            imageUrl = uploadRes.url;
         }
         const message=await Message.create({senderId,receiverId,text,image:imageUrl});
         const recieverSocketId=userSocketMap[receiverId];
